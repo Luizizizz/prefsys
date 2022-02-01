d818c9b035f8d8aba37e6ee76b0b84bd6ce9f989
Luiz <luizclaudiosfonseca@hotmail.com>
2022-02-01 11:38:06 -0300

    .

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="Estaticos\CSS\Estilo.css">
+    <title>Estoque do TI</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h4>
+        <img 
+        src="http://www.bertioga.sp.gov.br/wp-content/themes/bertioga/assets/images/logo-prefeitura-de-bertioga.png" 
+        alt=""
+        width= 140 px
+        height= 40 px
+        >
+    </h4>
+    </header>
+    <main class="principal">
+            <?php require_once('menu.php'); ?>
+        </main>
+    <footer class="rodape">
+        Prefeitura de Bertioga
+    </footer>
+</body>
+</html>
